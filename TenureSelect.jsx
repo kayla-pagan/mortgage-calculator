32f968a08e0a0a684dbc97337cd149003025b560
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-12-28 10:31:46 -0500

    import all items for mui select component

--- TenureSelect.jsx
+++ TenureSelect.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 
 export default function TenureSelect(){
     return (
