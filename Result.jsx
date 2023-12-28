1aab378f5f6dbfaa850cb3ff2e683541617ba5c8
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-12-28 13:24:15 -0500

    create calc for monthlyPayment and totalInterest
    save these calculations in a variable

--- Result.jsx
+++ Result.jsx
@@ -7,6 +7,13 @@ export default function Result({data}){
     const totalLoanMonths = loanTerm * 12
     const interestPerMonth = interestRate / 100 / 12
 
+    const monthlyPayment =
+        loanAmount * interestPerMonth * 
+        (Math.pow(1 + interestPerMonth, totalLoanMonths)) / 
+        (Math.pow(1 + interestPerMonth, totalLoanMonths) - 1)
+
+    const totalInterestGenerated = monthlyPayment * totalLoanMonths - loanAmount
+
     return (
         <h1>Result</h1>
     )
