fa16c2495687d75a929ce8d1aac06e558ed3ccef
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-12-28 16:29:48 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 import Navbar from "./Components/Navbar";
+import Result from "./Components/Result";
+import SliderSelect from "./Components/SliderSelect";
+import TenureSelect from "./Components/TenureSelect";
 
-function App(){
+export default function App(){
+
+  const [data, setData] = React.useState({
+    homeValue: 200000,
+    downPayment: 5000,
+    loanAmount: 195000,
+    interestRate: 6.5,
+    loanTerm: 30
+  })
+
   return (
     <div className="App">
       <Navbar />
+      <Container maxWidth="xl" sx={{mt:4}}>
+        <Grid container spacing={5} alignItems="center">
+          <Grid item xs={12} md={6}>
+            <SliderSelect data={data} setData={setData} />
+            <TenureSelect data={data} setData={setData} />
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Result data={data} />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   )
 }
-
-export default App;
