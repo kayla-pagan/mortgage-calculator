2830ddb23537b84c72b84b697d2e7a589126e306
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-12-29 19:13:42 -0500

    apply margin to bottom for sx on container

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export default function App(){
   return (
     <div className="App">
       <Navbar />
-      <Container maxWidth="xl" sx={{mt:4}}>
+      <Container maxWidth="xl" sx={{my:4}}>
         <Grid container spacing={5} alignItems="center">
           <Grid item xs={12} md={6}>
             <SliderSelect data={data} setData={setData} />
